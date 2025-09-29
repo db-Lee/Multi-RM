d1578e4a0cab17ad4d405215a29d2f9b2747acb0
db-Lee <markmarkhi@naver.com>
2025-09-29 11:39:41 +0200

    asdf

--- discriminative/get_reward.py
+++ discriminative/get_reward.py
@@ -115,8 +115,9 @@ def main():
         
         try:
             dataset = load_dataset(args.data_path, split=category)
+            dataset = [ d for d in dataset ]
         except:
-            with open(os.path.join(args.input_dir, f"{category}_dataset.json"), "r") as f:            
+            with open(os.path.join(args.input_dir, f"{category}.json"), "r") as f:            
                 dataset = json.load(f)        
         
         print(f"Dataset loaded: {len(dataset)} items")
